1f3d271d5316485a604699b2701f798aa63e678e
Christopher Chin <ctchin13@gmail.com>
2017-06-01 13:21:59 -0400

    Pointer motion playback.

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ extern crate time;
 mod uinput;
 
 use input::{AsRaw, Libinput, LibinputInterface};
-use input::Event::Keyboard;
+use input::Event::{Keyboard, Pointer};
 use input::event::Event;
 use input::event::KeyboardEvent::Key;
+use input::event::PointerEvent::Motion;
 use input::event::keyboard::{KeyboardEventTrait, KeyState};
+use input::event::pointer::PointerEventTrait;
 use libc::{c_char, c_int, c_void};
 use uinput::UInput;
 
@@ -63,7 +65,8 @@ fn replay_events(events: &Vec<Event>, uinput: &mut UInput) {
 
     for e in events {
         // Not ideal, but can't get time on generic Event.
-        let mut time = 0;
+        // Each event should assign this value to msec time
+        let mut time = prev_event_time;
 
         match e {
             &Keyboard(Key(ref key_event)) => {
@@ -79,10 +82,16 @@ fn replay_events(events: &Vec<Event>, uinput: &mut UInput) {
                     },
                 }
             },
-            _ => println!("Unknown event in event store!")
+            &Pointer(Motion(ref motion_event)) => {
+                time = motion_event.time_usec() / 1000;
+
+                uinput.rel_x(motion_event.dx() as i32);
+                uinput.rel_y(motion_event.dy() as i32);
+            },
+            _ => {},
         }
         // Sleep for event delta time then send event
-        if prev_event_time != 0 {
+        if prev_event_time != 0 && prev_event_time != time {
             std::thread::sleep(std::time::Duration::from_millis(time - prev_event_time));
         }
         prev_event_time = time;
@@ -90,6 +99,16 @@ fn replay_events(events: &Vec<Event>, uinput: &mut UInput) {
     }
 }
 
+fn mouse_to_home(home: (f64, f64), curr: (f64, f64), uinput: &mut UInput) {
+    println!("Attempting to move to {:?} from {:?}", home, curr);
+    let delta_x = home.0 - curr.0;
+    let delta_y = home.1 - curr.1;
+    println!("Moving {} x and {} y", delta_x as i32, delta_y as i32);
+
+    uinput.rel_x(delta_x as i32);
+    uinput.rel_y(delta_y as i32);
+    uinput.sync();
+}
 
 fn main() {
     let mut libinput = unsafe { libinput_from_udev() };
@@ -97,6 +116,8 @@ fn main() {
     let mut event_store = Vec::new();
 
     let mut recording = false;
+    let mut pointer_record_start = (0_f64, 0_f64);
+    let mut pointer_delta = (0_f64, 0_f64); // total accumulated change in mouse since start of program
 
     loop {
         if let Err(_) = libinput.dispatch() {
@@ -106,43 +127,58 @@ fn main() {
         while let Some(event) = libinput.next() {
             match event {
                 Keyboard(Key(key_event)) => {
-                        let key = key_event.key();
-                        let key_state = key_event.key_state();
+                    let key = key_event.key();
+                    let key_state = key_event.key_state();
 
-                        // Perhaps check if prev_event_time was 0;
+                    // Perhaps check if prev_event_time was 0;
 
-                        match uinput::Key::from(key as u8) {
-                            RECORD_KEY => {
-                                if key_state == KeyState::Released {
-                                    recording = !recording;
+                    match uinput::Key::from(key as u8) {
+                        RECORD_KEY => {
+                            if key_state == KeyState::Released {
+                                recording = !recording;
 
-                                    if recording {
-                                        // Flush the event_store
-                                        event_store.clear();
-                                        println!("Now recording.");
-                                    } else {
-                                        println!("Stopped recording.");
-                                    }
-
-                                    // Get current mouse position and create an AbsMove to it in
-                                    // event_store.
+                                if recording {
+                                    // Flush the event_store
+                                    event_store.clear();
+                                    pointer_record_start = pointer_delta;
+                                    println!("Now recording.");
+                                } else {
+                                    println!("Stopped recording.");
                                 }
-                            },
-                            REPLAY_KEY => {
-                                if key_state == KeyState::Released {
-                                    recording = false;
-                                    replay_events(&event_store, &mut uinput);
-                                }
-                            },
-                            _ => if recording {
-                                event_store.push(Keyboard(Key(key_event)));
-                            },
-                        }
+                            }
+                        },
+                        REPLAY_KEY => {
+                            if key_state == KeyState::Released {
+                                recording = false;
+                                mouse_to_home(pointer_record_start, pointer_delta, &mut uinput);
 
-                        //println!("Key {} {:?} [+{}ms]", key, key_state, (time - prev_event_time) / 1000);
+                                libinput.suspend();
+                                replay_events(&event_store, &mut uinput);
+                                libinput.resume();
+                            }
+                        },
+                        _ => if recording {
+                            event_store.push(Keyboard(Key(key_event)));
+                        },
+                    }
 
+                    //println!("Key {} {:?} [+{}ms]", key, key_state, (time - prev_event_time) / 1000);
                 },
-                _ => {},
+                Pointer(Motion(pointer_motion_event)) => {
+                    pointer_delta.0 += pointer_motion_event.dx();
+                    pointer_delta.1 += pointer_motion_event.dy();
+
+                    if recording {
+                        event_store.push(Pointer(Motion(pointer_motion_event)));
+                    }
+
+                    println!("Total {:?}", pointer_delta);
+                },
+                e => {
+                    if recording {
+                        event_store.push(e);
+                    }
+                },
             }
         }
     }
