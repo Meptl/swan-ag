0b8de53bb71cfbdd90f2be07d8a2cbd78347c0e7
Christopher Chin <ctchin13@gmail.com>
2017-06-03 01:33:19 -0400

    Init argument parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-#[macro_use] extern crate nix;
+extern crate argparse;
 extern crate input;
 extern crate libc;
 extern crate libudev_sys;
+#[macro_use] extern crate nix;
 extern crate time;
 
 mod uinput;
 
+use argparse::{ArgumentParser, Store, StoreTrue};
 use input::{AsRaw, Libinput, LibinputInterface};
 use input::Event::{Keyboard, Pointer};
 use input::event::Event;
@@ -41,6 +43,21 @@ extern fn close_restricted(fd: c_int, user_data: *mut c_void) {
     }
 }
 
+/// Command line options for this program.
+struct Options {
+    instant: bool,
+    speed: f64,
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Options {
+            instant: false,
+            speed: 1.0,
+        }
+    }
+}
+
 unsafe fn libinput_from_udev() -> Libinput {
     let udev = libudev_sys::udev_new();
     if udev.is_null() {
@@ -61,7 +78,7 @@ unsafe fn libinput_from_udev() -> Libinput {
 
 /// Replay events in the event store.
 /// Modifies the pointer position.
-fn replay_events(events: &Vec<Event>, uinput: &mut UInput) {
+fn replay_events(options: &Options, events: &Vec<Event>, uinput: &mut UInput) {
     println!("Replay!");
     let mut prev_event_time = 0;
     let mut pointer_err = (0_f64, 0_f64); // Total accumulated positional error
@@ -127,8 +144,9 @@ fn replay_events(events: &Vec<Event>, uinput: &mut UInput) {
         }
         // Sleep for event delta time then send event
         // Sometimes events become unordered and time is off.
-        if prev_event_time != 0 && prev_event_time < time {
-            std::thread::sleep(std::time::Duration::from_millis(time - prev_event_time));
+        if !options.instant && prev_event_time != 0 && prev_event_time < time {
+            let delay_ms = (time - prev_event_time) as f64 / options.speed;
+            std::thread::sleep(std::time::Duration::from_millis(delay_ms as u64));
         }
         prev_event_time = time;
 
@@ -137,10 +155,25 @@ fn replay_events(events: &Vec<Event>, uinput: &mut UInput) {
     }
 }
 
+fn parse_args(options: &mut Options) {
+    let mut ap = ArgumentParser::new();
+    ap.set_description("Record input events and replay them.");
+    ap.refer(&mut options.instant)
+      .add_option(&["-i", "--instant"], StoreTrue,
+                  "Replay events with no delay between them");
+    ap.refer(&mut options.speed)
+      .add_option(&["-s", "--speed"], Store,
+                  "Replay speed modifier (default: 1.0)");
+    ap.parse_args_or_exit();
+}
+
 fn main() {
     let mut libinput = unsafe { libinput_from_udev() };
     let mut uinput = uinput::UInput::new();
     let mut event_store = Vec::new();
+
+    let mut options = Options::default();
+    parse_args(&mut options);
 
     let mut recording = false;
 
@@ -177,7 +210,7 @@ fn main() {
                                 recording = false;
 
                                 libinput.suspend();
-                                replay_events(&event_store, &mut uinput);
+                                replay_events(&options, &event_store, &mut uinput);
                                 if libinput.resume().is_err() {
                                     panic!("Failed to resume libinput");
                                 }
